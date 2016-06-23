0a4f1f01c2b2f5209f1a43bc587431a36c860bde
Fernando Vitale <fpvvitale@gmail.com>
2016-06-23 19:41:23 -0300

    Added search input

--- app/controllers/pokemon_search_controller.rb
+++ app/controllers/pokemon_search_controller.rb
@@ -1,11 +1,11 @@
 class PokemonSearchController < ApplicationController
   def index
-    pokemon_name = params[:pokemon_name] unless params[:pokemon_name].nil?
+    pokemon_name = params[:pokemon][:name].titlecase unless (params[:pokemon].nil? || params[:pokemon][:name].nil?)
     @poke = Pokemon.where(name: pokemon_name).take if !pokemon_name.nil?
     @pokemon_image = File.basename(URI.parse(@poke.image).path) if !@poke.nil?
     if @poke.nil?
-      flash[:error] = "The pokemon #{pokemon_name} could not be found."
-      redirect_to pokemon_search_url(pokemon_name: Pokemon.first.name)
+      flash[:error] = "The pokémon #{pokemon_name} could not be found."
+      redirect_to pokemon_search_url(params: {pokemon: {name: Pokemon.first.name}})
     else
       @damage = damage @poke.type1, @poke.type2
     end
