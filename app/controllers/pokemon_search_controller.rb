b728f7e4e32310f1c7f68f14e35d4e7d73b480e6
Fernando Vitale <fpvvitale@gmail.com>
2016-06-24 16:50:07 -0300

    Changed to pick cdn image if no asset is found.

--- app/controllers/pokemon_search_controller.rb
+++ app/controllers/pokemon_search_controller.rb
@@ -2,7 +2,10 @@ class PokemonSearchController < ApplicationController
   def index
     pokemon_name = params[:pokemon][:name] unless (params[:pokemon].nil? || params[:pokemon][:name].nil?)
     @poke = Pokemon.where('lower(name) = ?', pokemon_name.downcase).take if !pokemon_name.nil?
-    @pokemon_image = File.basename(URI.parse(@poke.image).path)[0,3]+".png" if !@poke.nil?
+    if !@poke.nil?
+      file_name = File.basename(URI.parse(@poke.image).path)[0,3]+".png"
+      @pokemon_image = Rails.application.assets.find_asset("pokemon_images/"+file_name) ? "pokemon_images/"+file_name : @poke.image
+    end
     if @poke.nil?
       flash[:error] = "The pokémon #{pokemon_name} could not be found."
       redirect_to pokemon_search_url(params: {pokemon: {name: Pokemon.first.name}})
